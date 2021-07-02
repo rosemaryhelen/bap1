0d779b5d86fb524628c47c6ad3ae6c478caad710
Sarah Hamilton <50032548@belfastmet.ac.uk>
2021-07-02 16:08:33 +0100

    Added properties to StoreApp.cs Added table headings to FetchStoreApps.razor

--- Data/StoreApp.cs
+++ Data/StoreApp.cs
@@ -5,8 +5,24 @@ using Newtonsoft.Json;
     {
         [JsonProperty("id")]
         public int Id { get; set; }
-        
+
         [JsonProperty("name")]
         public string Name { get; set; }
+        
+        [JsonProperty("rating")]
+        public string Rating { get; set; }
+        
+        [JsonProperty("people")]
+        public string People { get; set; }
 
-    }
+        [JsonProperty("category")]
+        public String Category { get; set; }
+
+        [JsonProperty("date")]
+        public int Date { get; set; }
+
+        [JsonProperty("price")]
+        public int Price { get; set; }
+
+
+}
