89f4a7ed02012e1a8730d699b2d38b0581b33f8b
Cheryl McIntyre <50032549@belfastmet.ac.uk>
2021-07-03 16:05:33 +0100

    Updated AddModel class in Form.cs

--- Data/Form.cs
+++ Data/Form.cs
@@ -12,4 +12,17 @@ public class AddModel
     [Required]
     [StringLength(255, MinimumLength = 3, ErrorMessage = "Name should be between {2} and {1} characters long.")]
     public string Name { get; set; }
+
+    [Required]
+    [StringLength(1, ErrorMessage = "Enter a number between 1 and 5")]
+    public double Rating { get; set; }
+
+    [Required]
+    public int People { get; set; }
+
+    [Required]
+    public string Category { get; set; }
+
+    [Required]
+    public string Price { get; set; }
 }
