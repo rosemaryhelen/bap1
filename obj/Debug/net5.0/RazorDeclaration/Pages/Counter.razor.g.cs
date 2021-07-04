7bfa4173c4592165cfec2910fdbe7737182b2119
Rosemary Ogden <rhbogden@gmail.com>
2021-07-04 14:04:05 +0100

    Pagination coding added

--- obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
@@ -13,71 +13,85 @@ namespace BapBlazor.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 1 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 2 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 3 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 4 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 5 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 6 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 7 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 8 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 9 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using BapBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 10 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using BapBlazor.Shared;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 11 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
+using Syncfusion.Blazor;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 12 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
+using Syncfusion.Blazor.Charts;
 
 #line default
 #line hidden
@@ -91,7 +105,7 @@ using BapBlazor.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 9 "C:\Users\Student\source\repos\blazor\Pages\Counter.razor"
+#line 9 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\Pages\Counter.razor"
        
     private int currentCount = 0;
 
--- obj/Debug/net5.0/RazorDeclaration/Pages/FetchStoreApps.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/FetchStoreApps.razor.g.cs
@@ -13,84 +13,98 @@ namespace BapBlazor.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 2 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 2 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 3 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 4 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 5 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 6 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 7 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 8 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 9 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using BapBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Student\source\repos\blazor\_Imports.razor"
+#line 10 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
 using BapBlazor.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Student\source\repos\blazor\Pages\FetchStoreApps.razor"
+#line 11 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
+using Syncfusion.Blazor;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 12 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\_Imports.razor"
+using Syncfusion.Blazor.Charts;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 3 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\Pages\FetchStoreApps.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Student\source\repos\blazor\Pages\FetchStoreApps.razor"
+#line 8 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\Pages\FetchStoreApps.razor"
 using Newtonsoft.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Student\source\repos\blazor\Pages\FetchStoreApps.razor"
+#line 9 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\Pages\FetchStoreApps.razor"
 using Data;
 
 #line default
@@ -105,7 +119,7 @@ using Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 62 "C:\Users\Student\source\repos\blazor\Pages\FetchStoreApps.razor"
+#line 62 "C:\Users\rhbog\Source\Repos\rosemaryhelen\bap1\Pages\FetchStoreApps.razor"
        
 
     string responseBody = "";
